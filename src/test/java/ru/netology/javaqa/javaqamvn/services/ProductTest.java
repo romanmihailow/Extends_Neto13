97c4c96ccb1dcc65ba929d7e2021b7f420d0977b
romanmihailow <romanmihailow@gmail.com>
2025-02-25 11:31:34 +0500

    COVEREDRATIO100

--- src/test/java/ru/netology/javaqa/javaqamvn/services/ProductTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/ProductTest.java
@@ -1,9 +1,8 @@
 package ru.netology.javaqa.javaqamvn.services;
 
-import org.junit.Assert;
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test; // Для JUnit 5
+import static org.junit.jupiter.api.Assertions.assertEquals; // Для JUnit 5
 
 public class ProductTest {
 
@@ -13,7 +12,6 @@ public class ProductTest {
         Book book = new Book(1, "Book name_one", 100, "Author one");
         String expected = "Author one";
         String actual = book.getAuthor();
-        System.out.println("getAuthor() был вызван. Автор: " + actual);
         assertEquals(expected, actual);
     }
 
@@ -40,6 +38,17 @@ public class ProductTest {
         product.setName("Simpson");
         String actual = product.getName();
         String expected = "Simpson";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testProductConstructorWithName() {
+        // Создаем продукт с именем
+        Product product = new Product("Product name");
+
+        // Проверяем, что имя установлено правильно
+        String expected = "Product name";
+        String actual = product.getName();
         assertEquals(expected, actual);
     }
 
